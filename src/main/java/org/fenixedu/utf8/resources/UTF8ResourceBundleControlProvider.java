6d4138c5bb6538098729b3f1383f02d34a22ec04
Sérgio Silva <sergiofbsilva@gmail.com>
2018-11-17 01:20:01 +0000

    Add logging on init and bundle loading UTF8JRC-3 #resolve (#4)

--- src/main/java/org/fenixedu/utf8/resources/UTF8ResourceBundleControlProvider.java
+++ src/main/java/org/fenixedu/utf8/resources/UTF8ResourceBundleControlProvider.java
@@ -2,6 +2,8 @@ package org.fenixedu.utf8.resources;
 
 import java.util.ResourceBundle;
 import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.spi.ResourceBundleControlProvider;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -10,6 +12,8 @@ import java.util.stream.Stream;
  * Created by Sérgio Silva (hello@fenixedu.org).
  */
 public class UTF8ResourceBundleControlProvider implements ResourceBundleControlProvider {
+
+    private static final Logger logger = Logger.getLogger(UTF8ResourceBundleControlProvider.class.getName());
 
     private final UTF8ResourceBundleControl utf8ResourceBundleControl = new UTF8ResourceBundleControl();
 
@@ -20,13 +24,21 @@ public class UTF8ResourceBundleControlProvider implements ResourceBundleControlP
 
     public UTF8ResourceBundleControlProvider() {
         String utf8ResourceBaseNameValues = System.getProperty(RESOURCES_BASENAME_PROPERTY, DEFAULT_RESOURCES_BASENAME);
+        logger.info("Initializing UTF8 Resource Bundle for baseNames prefixes " + utf8ResourceBaseNameValues);
         RESOURCE_BASE_NAMES =
                 Stream.of(utf8ResourceBaseNameValues.split(",")).collect(Collectors.toSet());
     }
 
     @Override
     public ResourceBundle.Control getControl(final String baseName) {
-        return isUTF8BaseName(baseName) ? utf8ResourceBundleControl : null;
+        if (isUTF8BaseName(baseName)) {
+            if (logger.isLoggable(Level.FINE)) {
+                logger.fine("Loading UTF8 Bundle for " + baseName);
+            }
+            return utf8ResourceBundleControl;
+        } else {
+            return null;
+        }
     }
 
     private boolean isUTF8BaseName(final String baseName) {
